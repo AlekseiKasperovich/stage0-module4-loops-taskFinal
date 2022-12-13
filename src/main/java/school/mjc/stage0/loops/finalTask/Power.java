e5ca49251c860f4f7e599f90eedc8ab2567b4f0a
AlekseiKasperovich <AleksKasperovich@yandex.by>
2022-12-13 15:13:41 +0300

    commit

--- src/main/java/school/mjc/stage0/loops/finalTask/Power.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Power.java
@@ -1,8 +1,15 @@
 package school.mjc.stage0.loops.finalTask;
 
 public class Power {
-    public void printPower(int numberToPrint, int power) {
 
+    public void printPower(int numberToPrint, int power) {
+        int res = 0;
+        int temp = numberToPrint;
+        for (int i = 1; i < power; i++) {
+            res = temp * numberToPrint;
+            temp = res;
+        }
+        System.out.println(res);
     }
 
     public static void main(String[] args) {
--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -2,7 +2,21 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Pyramid {
     public void printPyramid(int cathetusLength) {
+        int k = 0;
 
+        for (int i = 1; i <= cathetusLength; ++i, k = 0) {
+            for (int space = 1; space <= cathetusLength - i; ++space) {
+                System.out.print("  ");
+            }
+
+            while (k != 2 * i - 1) {
+                System.out.print("* ");
+                ++k;
+            }
+
+            System.out.println();
+
+        }
     }
 
     public static void main(String[] args) {
